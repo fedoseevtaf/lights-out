9071ceecc8c78b7fed269169982f3f0414c27839
XMister2708 <timur27082006x@gmail.com>
2023-03-20 13:45:43 +0300

    Make button output in line

--- lights-out-bot/view.py
+++ lights-out-bot/view.py
@@ -1,13 +1,34 @@
 import telebot.types as tp
 
+from view_const import MESSAGES_BOT, TEXT_BUTTON
 
-class MessageConstructor():
+
+class MessageConstructor:
 
 	@classmethod
 	def make_text(cls, page_id, content):
-		return f'{page_id=}\n\n{content=}'
+		return MESSAGES_BOT[page_id]
 
 	@classmethod
 	def make_markup(cls, page_id, content):
-		return tp.InlineKeyboardMarkup()
+		markup = tp.ReplyKeyboardMarkup(resize_keyboard=True)
+		markup.add(tp.KeyboardButton(text_btn) for text_btn in TEXT_BUTTON[page_id])
+		if page_id == 'game':
+			markup = tp.InlineKeyboardMarkup(row_width=content[width])
+			markup.add(*(tp.InlineKeyboardButton(
+					'⬜' if btn else '⬛',
+					callback_data=(
+							'{"row":' +
+							str(ind // content[width]) +
+							', "col": ' +
+							str(ind % content[width]) + '}'
+					)
+				)
+				for ind, btn in enumerate(content[board])
+				)
+			)
+		return markup
 
+	@classmethod
+	def make_board(cls, page_id):
+		pass
